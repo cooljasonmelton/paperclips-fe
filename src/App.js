62a8917a08aae5a827b4a7fcbc742c2abc3acccf
cooljasonmelton <cooljasonmelton>
2020-08-20 00:35:28 -0500

    setting up menu and css for drop down

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 
 // styling
--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //router
 import { withRouter } from 'react-router-dom';
@@ -6,19 +6,34 @@ import { withRouter } from 'react-router-dom';
 //styling
 import './Menu.css';
 
-const Menu = () => {
+const Menu = props => {
+  const [openDrop, setOpenDrop] = useState(false)
+
+  const navigateSite = e => {
+    props.history.push(e.target.textContent.toLowerCase())
+  }
+
+  const setNavClass = () => {
+    const classArr = ["nav-item"]
+    if (openDrop) classArr.push('open')
+    return classArr.join(' ')
+  }
+
   return (
     <div className="Menu">
-      <div>
+      <div onClick={()=>setOpenDrop(!openDrop)}>
         <p>paperclips</p>
       </div>
+
+      <div className="drop-nav">
+        <div className={setNavClass()} onClick={e=>navigateSite(e)}>Write</div>
+        <div className={setNavClass()} onClick={e=>navigateSite(e)}>Analytics</div>
+      </div>
+
       
-      <select>
-        <option>Write</option>
-        <option>Analytics</option>
-      </select>
+
     </div>
   );
 }
 
-export default Menu;
+export default withRouter(Menu);
