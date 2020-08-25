8ef9a3770cd98c3312d09f2000264fd49271fb9b
cooljasonmelton <cooljasonmelton>
2020-08-25 16:05:34 -0500

    set up login route and css of login and app

---
+++ src/login/LoginContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// styling
+import './LoginContainer.css';
+
+// components
+
+const LoginContainer = () => {
+  return (
+    <div className="LoginContainer">
+
+    </div>
+  );
+}
+
+export default LoginContainer;
---
+++ src/login/SignUp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// styling
+import './LoginContainer.css';
+
+// components
+
+const SignUp = () => {
+  return (
+    <div className="SignUp">
+
+    </div>
+  );
+}
+
+export default SignUp;
