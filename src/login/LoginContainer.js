4af0f345f61f24aac07c90769249a4efdfe84d02
cooljasonmelton <cooljasonmelton>
2020-08-26 02:25:11 -0500

    setting up login container css

--- src/login/LoginContainer.js
+++ src/login/LoginContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // styling
 import './LoginContainer.css';
@@ -6,9 +6,19 @@ import './LoginContainer.css';
 // components
 
 const LoginContainer = () => {
+  const [signIn, setSignIn] = useState(false)
+
   return (
     <div className="LoginContainer">
-
+      <div className="banner-side cfb">
+        Banner
+        <button onClick={()=> setSignIn(!signIn)}> 
+          {signIn ? "Sign In" : "Create an Account"}
+        </button>
+      </div>
+      <div className="form-side cfb">
+        Form
+      </div>
     </div>
   );
 }
