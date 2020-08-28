a6c317c8ef380e6553a83eba12afe41d7b54f9e5
cooljasonmelton <cooljasonmelton>
2020-08-28 13:11:39 -0500

    finished login form css transitions

--- src/login/SignUp.js
+++ src/login/SignUp.js
@@ -18,14 +18,10 @@ const SignUp = () => {
           <input type="text" value="" />
         </label>
         <label>
-          <h5>Password:</h5>
-          <input type="password" value="" />
+          <h5>Word Count Goal:</h5>
+          <input type="number" value="" placeholder="500" />
         </label>
         <br/>
-        <label>
-          <h5>Email:</h5>
-          <input type="text" value="" />
-        </label>
         <label>
           <h5>Password:</h5>
           <input type="password" value="" />
