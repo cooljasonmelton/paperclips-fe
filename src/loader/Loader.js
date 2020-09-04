ed76ac5209462febf0dbdffaf09296b7c940d2f2
cooljasonmelton <cooljasonmelton>
2020-09-03 19:57:37 -0500

    styling loader

--- src/loader/Loader.js
+++ src/loader/Loader.js
@@ -6,7 +6,7 @@ import './Loader.css';
 const Loader = () => {
   return (
     <div className="Loader cfb">
-        <div className="spin-loader cfb">Loading</div>
+        <div className="spin-loader cfb"></div>
     </div>
   );
 }
