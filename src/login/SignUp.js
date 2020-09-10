7b0fcc8a37c0a03a570e1987cd4f9125e378fb79
cooljasonmelton <cooljasonmelton>
2020-09-10 14:25:01 -0500

    set up sign up form

--- src/login/SignUp.js
+++ src/login/SignUp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 // styling
 import './LoginContainer.css';
@@ -6,25 +6,39 @@ import './LoginContainer.css';
 // components
 
 const SignUp = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [goal, setGoal] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <div className="SignUp cfb">   
       <form className="signup-form">
         <label>
           <h5>Name:</h5>
-          <input type="text" value="" />
+          <input type="text" 
+            onChange={e=> setName(e.target.value)}
+            value={name} />
         </label>
         <label>
           <h5>Email:</h5>
-          <input type="text" value="" />
+          <input type="text"
+            onChange={e=>{setEmail(e.target.value)}}
+            value={email} />
         </label>
         <label>
           <h5>Word Count Goal:</h5>
-          <input type="number" value="" placeholder="500" />
+          <input type="number" 
+            onChange={e=>setGoal(e.target.value)}
+            value={goal} 
+            placeholder="500" />
         </label>
         <br/>
         <label>
           <h5>Password:</h5>
-          <input type="password" value="" />
+          <input type="password" 
+            onChange={e=>setPassword(e.target.value)}
+            value={password} />
         </label>
         <br/>
         <input className="submit-button" type="submit" value="sign up" />
